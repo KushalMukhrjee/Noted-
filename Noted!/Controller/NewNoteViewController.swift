fff698082ea1d9dc930aaa29cae1f302f7f28088
Kushal Mukherjee <45260559+KushalMukhrjee@users.noreply.github.com>
2019-05-27 15:43:58 +0530

    New note, deletion of note implemented

---
+++ Noted!/Controller/NewNoteViewController.swift
@@ -0,0 +1,109 @@
+//
+//  NewNoteViewController.swift
+//  Noted!
+//
+//  Created by Kushal Mukherjee on 26/05/19.
+//  Copyright © 2019 Kushal Mukherjee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class NewNoteViewController: UIViewController {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var titleOutlet: UITextField!
+    
+    
+    @IBOutlet weak var textOutlet: UITextView!
+    
+    
+    var selectedNote:Note?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let saveButton=UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveNote))
+        self.navigationItem.rightBarButtonItem=saveButton
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let note = selectedNote{
+            self.titleOutlet.text = note.title
+            self.textOutlet.text = note.text
+            
+        }
+    }
+    
+    
+    //MARK: - save note method
+    @objc func saveNote(){
+        var newNoteObj : Note?
+        if(selectedNote == nil){
+            newNoteObj=Note(context: context)
+        }
+        else{
+            newNoteObj=selectedNote
+        }
+        
+        if(titleOutlet.text?.count == 0 && textOutlet.text.count == 0){
+            self.navigationController?.popViewController(animated: true)
+            deleteNote(note: newNoteObj!)
+            
+        }
+        else{
+            
+            newNoteObj?.title=titleOutlet.text
+            newNoteObj?.text=textOutlet.text
+            
+            do{
+            try context.save()
+                print("Note saved")
+                
+            }
+            catch{
+                print("Error saving note :\(error)")
+            }
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        
+        
+    }
+    
+    //    MARK:- Delete note
+    
+    func deleteNote(note:Note){
+        context.delete(note)
+    }
+    
+    
+//    //MARK: - get note method
+//    
+//    func getNote(){
+//        
+//        let fetchNote : NSFetchRequest<Note> = Note.fetchRequest()
+//        
+//        
+//        
+//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.textOutlet.resignFirstResponder()
+        self.titleOutlet.resignFirstResponder()
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ Noted!/Controller/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  Noted!
+//
+//  Created by Kushal Mukherjee on 26/05/19.
+//  Copyright © 2019 Kushal Mukherjee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var notesArray:[Note]=[]
+    
+    let context=(UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableViewOutlet.delegate=self
+        tableViewOutlet.dataSource=self
+        
+        editButtonItem.action=#selector(editMethod)
+        self.navigationItem.leftBarButtonItem=editButtonItem
+        
+        
+//        self.navigationItem.leftBarButtonItem=self.editButtonItem
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getNotes()
+    }
+    
+    @objc func editMethod(){
+
+        if(editButtonItem.title=="Edit"){
+
+            self.tableViewOutlet.isEditing=true
+            self.editButtonItem.title="Done"
+        }
+        else{
+
+            self.tableViewOutlet.isEditing=false
+            self.editButtonItem.title="Edit"
+            
+        }
+        
+    }
+    
+    
+    func getNotes(){
+        
+        let fetchRequest:NSFetchRequest<Note>=Note.fetchRequest()
+        do{
+            notesArray = try context.fetch(fetchRequest)
+        }
+        catch{
+            print("Error fetching notes :\(error)")
+        }
+        
+        tableViewOutlet.reloadData()
+        
+        
+    }
+    
+    
+    
+    
+}
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+   
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=tableViewOutlet.dequeueReusableCell(withIdentifier: "notescell", for: indexPath)
+        
+        if(notesArray[indexPath.row].title != ""){
+        cell.textLabel?.text = notesArray[indexPath.row].title
+            cell.textLabel?.textColor=UIColor.cyan
+        }
+        else{
+            cell.textLabel?.text = notesArray[indexPath.row].text
+            cell.textLabel?.textColor=UIColor.cyan
+        }
+        
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "newnotesegue", sender: self)
+        tableViewOutlet.deselectRow(at: tableViewOutlet.indexPathForSelectedRow!, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == .delete){
+            print("Delete rows now")
+            
+            var deleteNoteObj = notesArray[indexPath.row]
+            context.delete(deleteNoteObj)
+            do{
+                try context.save()
+            }
+            catch{
+                print("Error while saving after delete : \(error)")
+            }
+            
+            notesArray.remove(at: indexPath.row)
+            tableViewOutlet.deleteRows(at: [indexPath], with: .fade)
+            
+            
+            
+            
+        }
+//        tableViewOutlet.reloadData()
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! NewNoteViewController
+        if let indexpath = tableViewOutlet.indexPathForSelectedRow{
+            destinationVC.selectedNote = notesArray[indexpath.row]
+        }
+    }
+    
+    
+    
+    
+    
+}
